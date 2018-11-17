7e35bc345bfc3bae8b2f627a7362850c1ce38326
Damian-Teodor Beles <33461406+damianbeles@users.noreply.github.com>
2018-11-17 11:17:22 +0200

    Removed unnecessary whitespaces

--- src/ro/damianteodorbeles/imagepanel/ImageFrameComponent.java
+++ src/ro/damianteodorbeles/imagepanel/ImageFrameComponent.java
@@ -7,15 +7,15 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class ImageFrameComponent extends JPanel {
-	
+
 	private final BufferedImage image;
-	
+
 	public ImageFrameComponent(final BufferedImage image, final Dimension size) {
 		final BufferedImage scaledImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_ARGB);
 		scaledImage.getGraphics().drawImage(image.getScaledInstance(size.width, size.height, BufferedImage.SCALE_SMOOTH), 0, 0, null);
 		this.image = scaledImage;
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics graphics) {
 		super.paintComponent(graphics);
