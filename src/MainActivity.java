4ab7ddeccd4bdc6ae8aa34cf5d0d7fbc6dc4ac7a
Damian-Teodor Beles <33461406+damianbeles@users.noreply.github.com>
2018-11-17 11:11:10 +0200

    Implemented functionality for choosing which image to be processed

---
+++ src/MainActivity.java
@@ -0,0 +1,46 @@
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import ro.damianteodorbeles.imagepanel.ImageFrame;
+
+public class MainActivity {
+	
+	static final int PREFERRED_WIDTH = 600;
+	static final int PREFERRED_HEIGHT = 450;
+	
+	static BufferedImage getImageFromFileChooser() {
+		BufferedImage image = null;
+		
+		final JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new FileNameExtensionFilter("Image Files (*.jpg) | (*.gif) | (*.png)", "jpg", "gif", "png"));
+        
+        final int dialogResult = fileChooser.showOpenDialog(null);
+        if(dialogResult == JFileChooser.APPROVE_OPTION) {
+        	try {
+        		image = ImageIO.read(fileChooser.getSelectedFile().getAbsoluteFile());
+        	}
+        	catch (IOException exception) {
+        		System.out.println("Couldn't read the file containing the image!");
+        	}
+        }
+        
+        return image;
+	}
+	
+	static BufferedImage processImage(final BufferedImage image) {
+		return null;
+	}
+	
+	public static void main(String args[]) {
+		ImageFrame originalImageFrame = new ImageFrame(new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT));
+		originalImageFrame.createImageWindow("Original Image", getImageFromFileChooser());
+		
+		ImageFrame processedImageFrame = new ImageFrame(new Dimension(PREFERRED_WIDTH, PREFERRED_HEIGHT));
+		processedImageFrame.createImageWindow("Processed Image", processImage(originalImageFrame.getImage()));
+	}
+}
---
+++ src/ro/damianteodorbeles/imagepanel/ImageFrame.java
@@ -0,0 +1,41 @@
+package ro.damianteodorbeles.imagepanel;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ImageFrame extends JPanel {
+	
+	private Dimension size;
+	private BufferedImage image;
+	
+	public ImageFrame(Dimension size) {
+		setSize(size);
+	}
+	
+	public void setSize(final Dimension size) {
+		this.size = size;
+	}
+	
+	public Dimension getSize() {
+		return new Dimension(this.size);
+	}
+	
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	public JFrame createImageWindow(final String title, final BufferedImage image) {
+		this.image = image;
+		
+		final JFrame frame = new JFrame(title);
+		frame.add(new ImageFrameComponent(image, size));
+		frame.setResizable(false);
+		frame.setSize(this.size);
+		frame.setVisible(true);
+		
+		return frame;
+	}
+}
