277badf05d619afa1bcb771fc95038e18efb3ab9
Damian-Teodor Beles <33461406+damianbeles@users.noreply.github.com>
2019-01-07 12:14:56 +0200

    Removed duplicate code from filterEngine by implementing Processor class

--- src/ro/damianteodorbeles/imageprocessing/FilterEngine.java
+++ src/ro/damianteodorbeles/imageprocessing/FilterEngine.java
@@ -13,33 +13,6 @@ public class FilterEngine {
 
 	private final int numberOfThreadsToProcessOn = 4;
 
-	public BufferedImage processImage(final BufferedImage image, Filter filter) {
-		switch (filter) {
-		case BOX_BLUR:
-			return applyKernel(image, new KernelFactory().BOX_BLUR());
-		case GAUSSIAN_BLUR:
-			return applyKernel(image, new KernelFactory().GAUSSIAN_BLUR());
-		case EDGE_DETECTION:
-			return applyKernel(image, new KernelFactory().EDGE_DETECTION());
-		case SHARPEN:
-			return applyKernel(image, new KernelFactory().SHARPEN());
-		case EMBOSS:
-			return applyKernel(image, new KernelFactory().EMBOSS());
-		case RED_COLORING:
-			return setColorFilter(image, ColorFilter.RED);
-		case GREEN_COLORING:
-			return setColorFilter(image, ColorFilter.GREEN);
-		case BLUE_COLORING:
-			return setColorFilter(image, ColorFilter.BLUE);
-		case GRAYSCALE:
-			return grayscale(image);
-		case NEGATIVE:
-			return negative(image);
-		default:
-			return image;
-		}
-	}
-
 	private BufferedImage copyImage(BufferedImage image) {
 		BufferedImage returnImage = new BufferedImage(image.getWidth(), image.getHeight(),
 				BufferedImage.TYPE_4BYTE_ABGR);
@@ -55,224 +28,170 @@ public class FilterEngine {
 		return returnImage;
 	}
 
-	private BufferedImage negative(final BufferedImage image) {
-		BufferedImage imageToProcess = copyImage(image);
-		final int imageWidth = imageToProcess.getWidth();
-		final int imageHeight = imageToProcess.getHeight();
-		final byte[] pixels = ((DataBufferByte) imageToProcess.getRaster().getDataBuffer()).getData();
+	private class Processor {
 
-		final byte[] newPixels = new byte[pixels.length];
+		public void applyNegative(final int startRow, final int endRow, final int startCol, final int endCol,
+				final int imageWidth, byte[] pixels, byte[] newPixels) {
+			for (int row = startRow; row < endRow; ++row) {
+				for (int col = startCol; col < endCol; ++col) {
+					int pos = row * 4 * imageWidth + col * 4;
 
-		ExecutorService grayscaleMultithread = Executors.newFixedThreadPool(numberOfThreadsToProcessOn);
-		CompletionService<Void> grayscaleMultithreadCompletition = new ExecutorCompletionService<Void>(
-				grayscaleMultithread);
-
-		for (int threadIndex = 0; threadIndex < numberOfThreadsToProcessOn; ++threadIndex) {
-			final int indexer = threadIndex;
-			grayscaleMultithreadCompletition.submit(new Callable<Void>() {
-				public Void call() {
-					final int startRow = indexer * (imageHeight / numberOfThreadsToProcessOn);
-					final int endRow = (indexer + 1) * (imageHeight / numberOfThreadsToProcessOn);
-					final int startCol = 0;
-					final int endCol = imageWidth;
-
-					for (int row = startRow; row < endRow; ++row) {
-						for (int col = startCol; col < endCol; ++col) {
-							int pos = row * 4 * imageWidth + col * 4;
-
-							newPixels[pos] = (byte) 0xFF;
-							for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
-								newPixels[pos + pixelNumber] = (byte) (255 - (int) (pixels[pos + pixelNumber] & 0xFF));
-						}
-					}
-					return null;
+					newPixels[pos] = (byte) 0xFF;
+					for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
+						newPixels[pos + pixelNumber] = (byte) (255 - (int) (pixels[pos + pixelNumber] & 0xFF));
 				}
-			});
-		}
-
-		for (int threadNumber = 0; threadNumber < numberOfThreadsToProcessOn; ++threadNumber) {
-			try {
-				grayscaleMultithreadCompletition.take();
-			} catch (InterruptedException e) {
-				System.out.println("Exception caught while processing the image on multiple threads.");
 			}
 		}
 
-		return createBufferedImageFromByteArray(newPixels, imageWidth, imageHeight);
-	}
+		public void applyGrayscale(final int startRow, final int endRow, final int startCol, final int endCol,
+				final int imageWidth, byte[] pixels, byte[] newPixels) {
+			for (int row = startRow; row < endRow; ++row) {
+				for (int col = startCol; col < endCol; ++col) {
+					int pos = row * 4 * imageWidth + col * 4;
 
-	private BufferedImage grayscale(final BufferedImage image) {
-		BufferedImage imageToProcess = copyImage(image);
-		final int imageWidth = imageToProcess.getWidth();
-		final int imageHeight = imageToProcess.getHeight();
-		final byte[] pixels = ((DataBufferByte) imageToProcess.getRaster().getDataBuffer()).getData();
+					final float blue = ((int) pixels[pos + 1]) & 0xFF;
+					final float green = ((int) pixels[pos + 2]) & 0xFF;
+					final float red = ((int) pixels[pos + 3]) & 0xFF;
+					byte gray = (byte) (0.299f * red + 0.587f * green + 0.114f * blue);
 
-		final byte[] newPixels = new byte[pixels.length];
-
-		ExecutorService grayscaleMultithread = Executors.newFixedThreadPool(numberOfThreadsToProcessOn);
-		CompletionService<Void> grayscaleMultithreadCompletition = new ExecutorCompletionService<Void>(
-				grayscaleMultithread);
-
-		for (int threadIndex = 0; threadIndex < numberOfThreadsToProcessOn; ++threadIndex) {
-			final int indexer = threadIndex;
-			grayscaleMultithreadCompletition.submit(new Callable<Void>() {
-				public Void call() {
-					final int startRow = indexer * (imageHeight / numberOfThreadsToProcessOn);
-					final int endRow = (indexer + 1) * (imageHeight / numberOfThreadsToProcessOn);
-					final int startCol = 0;
-					final int endCol = imageWidth;
-
-					for (int row = startRow; row < endRow; ++row) {
-						for (int col = startCol; col < endCol; ++col) {
-							int pos = row * 4 * imageWidth + col * 4;
-
-							final float blue = ((int) pixels[pos + 1]) & 0xFF;
-							final float green = ((int) pixels[pos + 2]) & 0xFF;
-							final float red = ((int) pixels[pos + 3]) & 0xFF;
-							byte gray = (byte) (0.299f * red + 0.587f * green + 0.114f * blue);
-
-							newPixels[pos] = (byte) 0xFF;
-							for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
-								newPixels[pos + pixelNumber] = gray;
-						}
-					}
-					return null;
+					newPixels[pos] = (byte) 0xFF;
+					for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
+						newPixels[pos + pixelNumber] = gray;
 				}
-			});
-		}
-
-		for (int threadNumber = 0; threadNumber < numberOfThreadsToProcessOn; ++threadNumber) {
-			try {
-				grayscaleMultithreadCompletition.take();
-			} catch (InterruptedException e) {
-				System.out.println("Exception caught while processing the image on multiple threads.");
 			}
 		}
 
-		return createBufferedImageFromByteArray(newPixels, imageWidth, imageHeight);
-	}
+		public void applyColorFilter(final int startRow, final int endRow, final int startCol, final int endCol,
+				final int imageWidth, byte[] pixels, byte[] newPixels, ColorFilter colorFilter) {
+			int offset;
+			if (colorFilter == ColorFilter.BLUE)
+				offset = 1;
+			else if (colorFilter == ColorFilter.GREEN)
+				offset = 2;
+			else
+				offset = 3;
 
-	private BufferedImage setColorFilter(final BufferedImage image, final ColorFilter colorFilter) {
-		BufferedImage imageToProcess = copyImage(image);
-		final int imageWidth = imageToProcess.getWidth();
-		final int imageHeight = imageToProcess.getHeight();
-		final byte[] pixels = ((DataBufferByte) imageToProcess.getRaster().getDataBuffer()).getData();
+			for (int row = startRow; row < endRow; ++row) {
+				for (int col = startCol; col < endCol; ++col) {
+					int pos = row * 4 * imageWidth + col * 4;
 
-		final byte[] newPixels = new byte[pixels.length];
-
-		int offset;
-		if (colorFilter == ColorFilter.BLUE)
-			offset = 1;
-		else if (colorFilter == ColorFilter.GREEN)
-			offset = 2;
-		else
-			offset = 3;
-
-		ExecutorService setColorFilterMultithread = Executors.newFixedThreadPool(numberOfThreadsToProcessOn);
-		CompletionService<Void> setColorFilterMultithreadCompletition = new ExecutorCompletionService<Void>(
-				setColorFilterMultithread);
-
-		for (int threadIndex = 0; threadIndex < numberOfThreadsToProcessOn; ++threadIndex) {
-			final int indexer = threadIndex;
-			setColorFilterMultithreadCompletition.submit(new Callable<Void>() {
-				public Void call() {
-					final int startRow = indexer * (imageHeight / numberOfThreadsToProcessOn);
-					final int endRow = (indexer + 1) * (imageHeight / numberOfThreadsToProcessOn);
-					final int startCol = 0;
-					final int endCol = imageWidth;
-
-					for (int row = startRow; row < endRow; ++row) {
-						for (int col = startCol; col < endCol; ++col) {
-							int pos = row * 4 * imageWidth + col * 4;
-
-							newPixels[pos] = (byte) 0xFF;
-							for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
-								newPixels[pos + pixelNumber] = (byte) 0;
-							newPixels[pos + offset] = pixels[pos + offset];
-						}
-					}
-					return null;
+					newPixels[pos] = (byte) 0xFF;
+					for (int pixelNumber = 1; pixelNumber < 4; ++pixelNumber)
+						newPixels[pos + pixelNumber] = (byte) 0;
+					newPixels[pos + offset] = pixels[pos + offset];
 				}
-			});
-		}
-
-		for (int threadNumber = 0; threadNumber < numberOfThreadsToProcessOn; ++threadNumber) {
-			try {
-				setColorFilterMultithreadCompletition.take();
-			} catch (InterruptedException e) {
-				System.out.println("Exception caught while processing the image on multiple threads.");
 			}
 		}
 
-		return createBufferedImageFromByteArray(newPixels, imageWidth, imageHeight);
-	}
+		public void applyKernel(final int startRow, final int endRow, final int startCol, final int endCol,
+				final int imageWidth, byte[] pixels, byte[] newPixels, final IKernel kernel) {
+			final float[] kernelMatrix = kernel.getKernel();
+			final int kernelWidth = kernel.getWidth();
+			final int kernelHeight = kernel.getHeight();
+			final int[] origin = kernel.getOrigin();
 
-	private BufferedImage applyKernel(final BufferedImage image, final IKernel kernel) {
+			final int startFromRow = startRow - origin[0];
+			final int endAtRow = endRow - origin[0];
+			final int startFromCol = startCol - origin[1];
+			final int endAtCol = endCol - origin[1];
+
+			for (int row = startFromRow; row < endAtRow; ++row) {
+				for (int col = startFromCol; col < endAtCol; ++col) {
+					float redSum = 0;
+					float greenSum = 0;
+					float blueSum = 0;
+					for (int npos = 0; npos < kernelMatrix.length; ++npos) {
+						int relativeRow = row + (kernelWidth - npos / kernelWidth - 1);
+						int relativeCol = col + (kernelWidth - npos % kernelWidth - 1);
+						if (relativeRow < startRow)
+							relativeRow += kernelHeight;
+						else if (relativeRow >= endRow)
+							relativeRow -= kernelHeight;
+						if (relativeCol < startCol)
+							relativeCol += kernelWidth;
+						else if (relativeCol >= endCol)
+							relativeCol -= kernelWidth;
+
+						int pos = (relativeRow * 4 * imageWidth) + (relativeCol * 4) + 1;
+						blueSum += kernelMatrix[npos] * ((int) pixels[pos++] & 0xFF);
+						greenSum += kernelMatrix[npos] * ((int) pixels[pos++] & 0xFF);
+						redSum += kernelMatrix[npos] * ((int) pixels[pos] & 0xFF);
+					}
+
+					int pos = ((row + origin[0]) * 4 * imageWidth) + ((col + origin[1]) * 4);
+					newPixels[pos++] = (byte) 0xFF;
+					newPixels[pos++] = (byte) blueSum;
+					newPixels[pos++] = (byte) greenSum;
+					newPixels[pos] = (byte) redSum;
+				}
+			}
+		}
+	};
+
+	public BufferedImage processImage(final BufferedImage image, Filter filter) {
 		BufferedImage imageToProcess = copyImage(image);
-		final float[] kernelMatrix = kernel.getKernel();
-		final int kernelWidth = kernel.getWidth();
-		final int kernelHeight = kernel.getHeight();
-		final int[] origin = kernel.getOrigin();
-
 		final int imageWidth = imageToProcess.getWidth();
 		final int imageHeight = imageToProcess.getHeight();
-
-		if (imageWidth < kernelWidth || imageHeight < kernelHeight) {
-			System.out.println("Can't process the image with the given kernel. Kernel too big.");
-			return null;
-		}
-
 		final byte[] pixels = ((DataBufferByte) imageToProcess.getRaster().getDataBuffer()).getData();
 
 		final byte[] newPixels = new byte[pixels.length];
 
-		ExecutorService applyKernelMultithread = Executors.newFixedThreadPool(numberOfThreadsToProcessOn);
-		CompletionService<Void> applyKernelMultithreadCompletition = new ExecutorCompletionService<Void>(
-				applyKernelMultithread);
+		ExecutorService processImageMultithread = Executors.newFixedThreadPool(numberOfThreadsToProcessOn);
+		CompletionService<Void> processImageMultithreadCompletition = new ExecutorCompletionService<Void>(
+				processImageMultithread);
 
 		for (int threadIndex = 0; threadIndex < numberOfThreadsToProcessOn; ++threadIndex) {
 			final int indexer = threadIndex;
-			applyKernelMultithreadCompletition.submit(new Callable<Void>() {
+			processImageMultithreadCompletition.submit(new Callable<Void>() {
 				public Void call() {
 					final int startRow = indexer * (imageHeight / numberOfThreadsToProcessOn);
 					final int endRow = (indexer + 1) * (imageHeight / numberOfThreadsToProcessOn);
 					final int startCol = 0;
 					final int endCol = imageWidth;
 
-					final int startFromRow = startRow - origin[0];
-					final int endAtRow = endRow - origin[0];
-					final int startFromCol = startCol - origin[1];
-					final int endAtCol = endCol - origin[1];
-
-					for (int row = startFromRow; row < endAtRow; ++row) {
-						for (int col = startFromCol; col < endAtCol; ++col) {
-							float redSum = 0;
-							float greenSum = 0;
-							float blueSum = 0;
-							for (int npos = 0; npos < kernelMatrix.length; ++npos) {
-								int relativeRow = row + (kernelWidth - npos / kernelWidth - 1);
-								int relativeCol = col + (kernelWidth - npos % kernelWidth - 1);
-								if (relativeRow < startRow)
-									relativeRow += kernelHeight;
-								else if (relativeRow >= endRow)
-									relativeRow -= kernelHeight;
-								if (relativeCol < startCol)
-									relativeCol += kernelWidth;
-								else if (relativeCol >= endCol)
-									relativeCol -= kernelWidth;
-
-								int pos = (relativeRow * 4 * imageWidth) + (relativeCol * 4) + 1;
-								blueSum += kernelMatrix[npos] * ((int) pixels[pos++] & 0xFF);
-								greenSum += kernelMatrix[npos] * ((int) pixels[pos++] & 0xFF);
-								redSum += kernelMatrix[npos] * ((int) pixels[pos] & 0xFF);
-							}
-
-							int pos = ((row + origin[0]) * 4 * imageWidth) + ((col + origin[1]) * 4);
-							newPixels[pos++] = (byte) 0xFF;
-							newPixels[pos++] = (byte) blueSum;
-							newPixels[pos++] = (byte) greenSum;
-							newPixels[pos] = (byte) redSum;
-						}
+					switch (filter) {
+					case BOX_BLUR:
+						new Processor().applyKernel(startRow, endRow, startCol, endCol, imageWidth, pixels, newPixels,
+								new KernelFactory().BOX_BLUR());
+						break;
+					case GAUSSIAN_BLUR:
+						new Processor().applyKernel(startRow, endRow, startCol, endCol, imageWidth, pixels, newPixels,
+								new KernelFactory().GAUSSIAN_BLUR());
+						break;
+					case EDGE_DETECTION:
+						new Processor().applyKernel(startRow, endRow, startCol, endCol, imageWidth, pixels, newPixels,
+								new KernelFactory().EDGE_DETECTION());
+						break;
+					case SHARPEN:
+						new Processor().applyKernel(startRow, endRow, startCol, endCol, imageWidth, pixels, newPixels,
+								new KernelFactory().SHARPEN());
+						break;
+					case EMBOSS:
+						new Processor().applyKernel(startRow, endRow, startCol, endCol, imageWidth, pixels, newPixels,
+								new KernelFactory().EMBOSS());
+						break;
+					case BLUE_COLORING:
+						new Processor().applyColorFilter(startRow, endRow, startCol, endCol, imageWidth, pixels,
+								newPixels, ColorFilter.BLUE);
+						break;
+					case GREEN_COLORING:
+						new Processor().applyColorFilter(startRow, endRow, startCol, endCol, imageWidth, pixels,
+								newPixels, ColorFilter.GREEN);
+						break;
+					case RED_COLORING:
+						new Processor().applyColorFilter(startRow, endRow, startCol, endCol, imageWidth, pixels,
+								newPixels, ColorFilter.RED);
+						break;
+					case GRAYSCALE:
+						new Processor().applyGrayscale(startRow, endRow, startCol, endCol, imageWidth, pixels,
+								newPixels);
+						break;
+					case NEGATIVE:
+						new Processor().applyNegative(startRow, endRow, startCol, endCol, imageWidth, pixels,
+								newPixels);
+						break;
+					default:
+						break;
 					}
 					return null;
 				}
@@ -281,7 +200,7 @@ public class FilterEngine {
 
 		for (int threadNumber = 0; threadNumber < numberOfThreadsToProcessOn; ++threadNumber) {
 			try {
-				applyKernelMultithreadCompletition.take();
+				processImageMultithreadCompletition.take();
 			} catch (InterruptedException e) {
 				System.out.println("Exception caught while processing the image on multiple threads.");
 			}
